02ceaab1f0b85ad7897bc8a63233742abf50dd13
bahadir11 <bahadirkarakuskk99@gmail.com>
2023-12-23 12:14:14 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// * nigga
+// *? is u stupid
+// *! this shit important nigga
+//todo unutma ki yapman gerekeni
+
+console.log("heyy how yall doinggg");
+
+document.write("nigga <br>");
+document.writeln("la kardeş");
+document.write(5.5);
+
+console.log("")
+let a = 5;
+let b = 12;
+console.log(a+b);
+// console.log(document.location.host);
+// alert("sum of two integers "+ (a+b));
+function adamOl(){
+    var adam = "adam ol lan";
+    console.log(adam);
+} 
+adamOl();
+const user = {
+    username :"baho",
+    password: "123"
+}
+var a1 = 5;
+a1=66;
+console.log(a1);
